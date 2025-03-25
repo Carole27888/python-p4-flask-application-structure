246531a9a1a2c179eb3573c8a98a71482d314aa7
carole27888 <cmutemi27@gmail.com>
2025-03-25 09:47:51 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -3,3 +3,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+# append to server/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+# append to server/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
